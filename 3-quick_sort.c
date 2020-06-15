1904ee25a72b6c3f7c94313ee682520d79e3d3e8
Myduzo <sahli.youssef@outlook.com>
2020-06-15 14:17:59 +0100

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,26 +9,26 @@
  */
 int partition(int *array, int left, int right, size_t size)
 {
-    int x = left, y = left, pivot = array[right], tmp;
+	int x = left, y = left, pivot = array[right], tmp;
 
 	while (y < right)
 	{
 		if (array[y] <= pivot)
 		{
-            tmp = array[x];
-            array[x] = array[y];
-            array[y] = tmp;
+			tmp = array[x];
+			array[x] = array[y];
+			array[y] = tmp;
 			if (x != y)
 				print_array(array, size);
 			x++;
 		}
-        y++;
+		y++;
 	}
 	if (x != y)
 	{
 		tmp = array[x];
-        array[x] = array[y];
-        array[y] = tmp;
+		array[x] = array[y];
+		array[y] = tmp;
 		print_array(array, size);
 	}
 	return (x);
@@ -44,14 +44,14 @@ int partition(int *array, int left, int right, size_t size)
  */
 void before_quicksort(int *array, int left, int right, size_t size)
 {
-    int location;
+	int location;
 
-    if (left < right)
-    {
-        location = partition(array, left, right, size);
-        before_quicksort(array, left, location - 1, size);
-        before_quicksort(array, location + 1, right, size);
-    }
+	if (left < right)
+	{
+		location = partition(array, left, right, size);
+		before_quicksort(array, left, location - 1, size);
+		before_quicksort(array, location + 1, right, size);
+	}
 }
 
 
@@ -62,5 +62,5 @@ void before_quicksort(int *array, int left, int right, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    before_quicksort(array, 0, (int)size - 1, size);
+	before_quicksort(array, 0, (int)size - 1, size);
 }
