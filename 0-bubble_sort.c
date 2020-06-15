5112c8a24b4a39a1dbf2a50a5da680090b7ac462
Myduzo <sahli.youssef@outlook.com>
2020-06-15 10:06:21 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * bubble_sort - Bubble sort algorithm
+ * @array: array of integers
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t pass = 0, idx = 0;
+	int temp, flag;
+
+	while (pass < size - 1)
+	{
+		flag = 0;
+		while (idx < size - 1)
+		{
+			if (array[idx] > array[idx + 1])
+			{
+				temp = array[idx + 1];
+				array[idx + 1] = array[idx];
+				array[idx] = temp;
+				flag = 1;
+				print_array(array, size);
+			}
+			idx++;
+		}
+		if (flag == 0)
+			break;
+		pass++;
+		idx = 0;
+	}
+}
