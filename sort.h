a33d7a5b0c88cb4163926592a4721eb40916dc36
Myduzo <sahli.youssef@outlook.com>
2020-06-17 20:27:12 +0100

    library

--- sort.h
+++ sort.h
@@ -26,6 +26,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void before_quicksort(int *array, int left, int right, size_t size);
 int partition(int *array, int left, int right, size_t size);
-
+void shell_sort(int *array, size_t size);
 
 #endif
