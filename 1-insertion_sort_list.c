5f253d7fe71668050e97827cfb15887be7da695f
Myduzo <sahli.youssef@outlook.com>
2020-06-18 10:50:33 +0100

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * insertion_sort - Insertion sort algorithm
+ * @list : list of integers
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp = *list, *curr = tmp->next, *left;
+
+	if (!list || !(*list))
+		return;
+
+	while (curr)
+	{
+		left = curr;
+		while (left->prev)
+		{
+			tmp = left->prev;
+			if (tmp->n > left->n)
+			{
+
+				tmp->next = left->next;
+				if (!curr->next)
+					tmp->next = NULL;
+				else
+					left->next->prev = tmp;
+
+				if (!tmp->prev)
+					*list = left;
+				else
+					tmp->prev->next = left;
+				left->prev = tmp->prev;
+				left->next = tmp;
+				tmp->prev = left;
+				print_list(*list);
+				continue;
+			}
+			left = left->prev;
+		}
+		curr = curr->next;
+	}
+}
